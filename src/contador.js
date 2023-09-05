7358afc9dafa40a95974053282f24c57aadf0a7b
Denilson Fiesta Inocente <denilson.fiesta@udb.edu.bo>
2023-09-05 10:08:57 -0400

    La frase <denilson> , no retorna ocurrencias -> denilson: 7

--- src/contador.js
+++ src/contador.js
@@ -1,9 +1,18 @@
 module.exports = class Contador{
 
     constructor(cadena){
+        this.mapita = {};
+        this.mapear(cadena);
     }
 
-    verificarOcurrencias( mapita){
+    mapear(cadena){
+        this.mapita[cadena] = 1;
+    }
+
+    verificarOcurrencias(palabras, cantidad){
+        for(let i = 0; i < palabras.length; i++){
+            if( this.mapita[palabras[i]] != cantidad[i]) return false;
+        }
         return true;
     }
 
--- src/contador.test.js
+++ src/contador.test.js
@@ -3,7 +3,17 @@ import Contador from "./contador";
 describe("Contador de ocurrencias", () => {
   it(" La frase \"denilson\", retorna ocurrencias -> denilson: 1 ", () => {
     const cont = new Contador("denilson");
-    let esperado = {"denilson":1};
-    expect(cont.verificarOcurrencias(esperado)).toEqual(true);
+    let palabras = ["denilson"];
+    let cantidad = [1];
+    expect(cont.verificarOcurrencias(palabras, cantidad)).toEqual(true);
   });
+
+  it(" La frase \"denilson\", no retorna ocurrencias -> denilson: 7 ", () => {
+    const cont = new Contador("denilson");
+    let palabras = ["denilson"];
+    let cantidad = [7];
+
+    expect(cont.verificarOcurrencias(palabras, cantidad)).toEqual(false);
+  });
+
 });
